1d778a649e0485b06ff2bcc228377d7b235a289f
yordysh <yordysh28@gmail.com>
2023-01-18 17:42:32 -0500

    Add Repository - Method findAll

---
+++ Infraestructure/Repositories/Implementations/EditorialRepository.cs
@@ -0,0 +1,24 @@
+﻿using Domain;
+using Infrastructure.Context;
+using Infrastructure.Repositories.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories.Implementations
+{
+    public class EditorialRepository : IEditorialRespository
+    {
+        private readonly ApplicationDbContext _context;
+        public EditorialRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IList<Editorial>> findAll()
+       => await _context.Editoriales.OrderByDescending(e=>e.Id).ToListAsync();
+    }
+}
