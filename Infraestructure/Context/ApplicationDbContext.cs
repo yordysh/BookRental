923dbf49341b8b432415d3d40a3f2bb14351caf5
yordysh <yordysh28@gmail.com>
2023-01-18 16:26:33 -0500

    AddEditorialMap

---
+++ Infraestructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Domain;
+using Infrastructure.ModelMaps;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DBConnection"));
+        }
+
+        public DbSet<Editorial> Editoriales { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new EditorialMap());
+        }
+    }
+}
