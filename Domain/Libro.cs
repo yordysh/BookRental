6c80ef35cfe28dc139347ee63893ff4dc0638585
yordy <yordysh28@gmail.com>
2023-03-02 12:34:53 -0500

    Libro - Model, Map and Context

---
+++ Domain/Libro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+        public string Isbn { get; set; }
+        public string Titulo { get; set; }
+        public string Autores { get; set; }
+        public string Edicion { get; set; }
+        public int Anio { get; set; }
+        public int IdEditorial { get; set; }
+         public DateTime FechaRegistro { get; set; } = DateTime.UtcNow;
+        public int Estado { get; set; } = 1;
+    }
+}
