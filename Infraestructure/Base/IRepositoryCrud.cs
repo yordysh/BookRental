a76f978f1f4bb7c8e6480e784e770cda46b6e70f
yordy <yordysh28@gmail.com>
2023-03-02 13:36:13 -0500

    refactoring repository inteerfaces

---
+++ Infraestructure/Base/IRepositoryCrud.cs
@@ -0,0 +1,13 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Base
+{
+    public interface IRepositoryCrud<T, K> : IRepositoryBase<T, K>, IRepositoryEditable<T, K>
+    {
+    }
+}
