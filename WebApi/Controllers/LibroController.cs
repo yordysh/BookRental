fb5682f4b1c575e0c178bba57417e3c3eec7d42e
yordy <yordysh28@gmail.com>
2023-03-11 14:29:30 -0500

    Add Controller Libro

---
+++ WebApi/Controllers/LibroController.cs
@@ -0,0 +1,64 @@
+﻿using Application.Dtos.Editoriales;
+using Application.Dtos.Libros;
+using Application.Services.Abstractions;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibroController : Controller
+    {
+        private readonly ILibroService _libroService;
+        public LibroController(ILibroService libroService)
+        {
+            _libroService = libroService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<LibroDto>> Get()
+            => await _libroService.FindAll();
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(LibroDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<Results<NotFound, Ok<LibroDto>>> Get(int id)
+        {
+            var response = await _libroService.Find(id);
+            if (response == null) return TypedResults.NotFound();
+            return TypedResults.Ok(response);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(LibroDto))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<Results<BadRequest, Ok<LibroDto>>> Post([FromBody] LibroFormDto request)
+        {
+            var response = await _libroService.Create(request);
+            if (response == null) return TypedResults.BadRequest();
+            return TypedResults.Ok(response);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(LibroDto))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<Results<BadRequest, NotFound, Ok<LibroDto>>> Put(int id, [FromBody] LibroFormDto request)
+        {
+            var response = await _libroService.Edit(id, request);
+            if (response == null) return TypedResults.NotFound();
+            return TypedResults.Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(LibroDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<Results<NotFound, Ok<LibroDto>>> Delete(int id)
+        {
+            var response = await _libroService.EnableOrDisable(id);
+            if (response == null) return TypedResults.NotFound();
+            return TypedResults.Ok(response);
+        }
+    }
+}
